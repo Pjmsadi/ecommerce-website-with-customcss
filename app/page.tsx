3334ab27384a6b3c6115a66889cbb5e24041f1ce
Pjmsadi <eshaalZahrah5@gmail.com>
2024-12-04 00:43:37 +0500

    'message

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import HomePage from "@/app/home/page";
-import Footer from "@/component/footer";
 
 export default function Home() {
   return (
     <div>
       <HomePage />
-      <Footer />
+  
     </div>
   );
 }
